e9dd0545f506705b1be01171e211cf02a007f333
Lovell Felix <lovell2juicy@yahoo.com>
2020-10-30 10:58:26 -0500

    added environment variables validation

--- libs/config.py
+++ libs/config.py
@@ -9,7 +9,13 @@ env_path = Path('.') / '.env'
 if env_path:
     load_dotenv(dotenv_path=env_path)
 
+REQUIRED_ENV_VARS = ["MONGO_URI_SOURCE", "MONGO_SOURCE_DATABASE", "MONGO_SOURCE_COLLECTION", "MONGO_URI_DESTINATION", "MONGO_DESTINATION_DATABASE", "MONGO_DESTINATION_COLLECTION"]
 
+for var in REQUIRED_ENV_VARS:
+    if var not in environ:
+        raise EnvironmentError("Failed because {} is not set.".format(var))
+
+DELETE_SOURCE = environ.get("DELETE_SOURCE", False)
 MONGO_URI_SOURCE = environ["MONGO_URI_SOURCE"]
 MONGO_SOURCE_DATABASE = environ["MONGO_SOURCE_DATABASE"]
 MONGO_SOURCE_COLLECTION = environ["MONGO_SOURCE_COLLECTION"]
